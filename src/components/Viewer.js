d551dc0edb58bbcc0bfc297b59d9933e7f8bb5c6
cherrykot <kot.aleksei43@gmail.com>
2019-06-06 11:50:17 +0300

    fix PhoneViewer (Gallery Working)

---
+++ src/components/Viewer.js
@@ -0,0 +1,44 @@
+import React from 'react';
+
+export class Viewer extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        mainPhoto: this.props.phone.images[0]
+      }
+  
+    }
+    handleClick(idx) {
+      console.log(idx);
+      this.setState({
+        mainPhoto: this.props.phone.images[idx]
+      })
+    }
+    render() {
+      
+      const { onBack, phone: { name, description, images } } = this.props,
+            { mainPhoto } = this.state;
+      console.log(this.props);
+      return (
+        <div>
+          <img className="phone" src={mainPhoto} alt='Main_photoPhone' />
+          <button onClick={onBack}>Back</button>
+          <button>Add to basket</button>
+  
+          <h1>{name}</h1>
+          <p>{description}</p>
+  
+          <ul className="phone-thumbs">
+            { images.map((imageUrl,idx) => (
+              <li key={idx}>
+                <img 
+                  src={imageUrl} 
+                  alt='photoPhones' 
+                  onClick={() => this.handleClick(idx)}/>
+              </li>
+            )) }
+          </ul>
+      </div>
+      )
+    }
+  };
