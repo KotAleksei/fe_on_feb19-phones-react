6ca0fd104a98493958ecffb0ade8f25d0c803bd4
cherrykot <kot.aleksei43@gmail.com>
2019-06-06 15:16:53 +0300

    now we can Remove items - WOOOW

--- src/components/ShoppingCart.js
+++ src/components/ShoppingCart.js
@@ -1,16 +1,31 @@
 import React from 'react';
 
-const ShoppingCart = () => {
-  return (
-    <section>
-      <p>Shopping Cart</p>
-      <ul>
-        <li>Phone 1 <button>x</button></li>
-        <li>Phone 2 <button>x</button></li>
-        <li>Phone 3 <button>x</button></li>
-      </ul>
-    </section>
-  );
+class ShoppingCart extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      items: []
+    }
+  }
+
+  render(){
+    const { removeItem } = this.props;
+    return (
+      <section>
+        <p>Shopping Cart</p>
+        <ul>
+          {
+            this.props.cards && this.props.cards.map((el,idx) => (
+              <li key={idx}>
+                {el}
+                <button onClick={() => removeItem(idx)}>Remove</button>
+              </li>
+            ))
+          }
+        </ul>
+      </section>
+    );
+  }
 };
 
 export default ShoppingCart;
