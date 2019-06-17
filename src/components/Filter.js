0e47bbeee70598a2363d80d5486f10740f28a11b
cherrykot <kot.aleksei43@gmail.com>
2019-06-17 19:32:54 +0300

    refactoring search logic

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,22 +1,39 @@
 import React from 'react';
 
-const Filter = (props) => {
-  return (
-    <section>
-      <p>
-        Search:
-        <input onChange={el => props.searchEl(el.target.value)}/>
-      </p>
-
-      <p>
-        Sort by:
-        <select onChange={el => props.sortCards(el.target.value)} >
-          <option value="name" >Alphabetical</option>
-          <option value="age">Newest</option>
-        </select>
-      </p>
-    </section>
-  );
+class Filter extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      searchEl: ''
+    }
+  }
+  handleSubmit = el => {
+    el.preventDefault();
+    this.props.searchEl(this.state.searchEl);
+  }
+  handleChange = el => {
+    this.setState({
+      searchEl: el.target.value
+    })
+  }
+  render() {
+    return (
+      <section>
+          Search:
+          <form onSubmit={el => this.handleSubmit(el)}>
+            <input onChange={el => this.handleChange(el)} />
+            <button style={{marginBottom: '20px'}}>Search</button>
+          </form>
+        <p>
+          Sort by:
+          <select onChange={el => this.props.sortCards(el.target.value)} >
+            <option value="name" >Alphabetical</option>
+            <option value="age">Newest</option>
+          </select>
+        </p>
+      </section>
+    );
+  }
 };
 
 export default Filter;
